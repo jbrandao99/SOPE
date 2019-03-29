2952e5b7bc3b5c7dc7ef6c6488c16e80f87270b1
Jota <jbrandao@live.com.pt>
2019-03-29 15:12:58 +0000

    Started Project

---
+++ Aulas Práticas/Aula 1/p4a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+
+    for(unsigned int i = 0; i < atoi(argv[2]); i++)
+    {
+            printf("Hello %s!\n", argv[1]);
+    }
+
+    return 0;
+}
