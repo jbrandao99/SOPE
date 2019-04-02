61b2b85f9e4d93b5001df4ec4959ccdbaca25eb1
Jota <jbrandao@live.com.pt>
2019-04-02 12:22:58 +0100

    error filetype

--- Projeto/forensic.c
+++ Projeto/forensic.c
@@ -5,12 +5,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+
+char (*filetype(char * argv));
 
 int main(int argc, char *argv[]) {
 
     struct stat buf;
     char tm[20]; //time modified
     char tc[20]; //time created
+    char* type;
 
     if(argc == 2)
     {
@@ -21,6 +25,8 @@ int main(int argc, char *argv[]) {
 
         printf("%s,", argv[1]);
         printf("%ld,",buf.st_size);
+        type = filetype(argv[1]);
+        printf("%s,",type);
         printf((buf.st_mode & S_IRUSR) ? "r" : "");
         printf((buf.st_mode & S_IWUSR) ? "w" : "");
         printf((buf.st_mode & S_IXUSR) ? "x" : ",");
@@ -39,4 +45,44 @@ int main(int argc, char *argv[]) {
     
 
     return 0;
+}
+
+char (*filetype(char * argv))
+{
+    int link[2];
+    __pid_t pid;
+    char foo[4096];
+    char final[4096];
+    pipe(link);
+    pid = fork();
+
+    if (pid == 0)
+    {
+        dup2(link[1], STDOUT_FILENO);
+        close(link[0]);
+        close(link[1]);
+        execlp("file", "file", argv, NULL);
+    }
+    else
+    {
+
+        close(link[1]);
+        read(link[0], foo, sizeof(foo));
+        for(int i=0; i<(strlen(argv)+2); i++){
+            foo[i]='0';
+        }
+        int k=0;
+        int t=0;
+        while(k<strlen(foo)){
+            if(foo[k]!='0'){
+                final[t]=foo[k];
+                t++;
+                k++;
+                continue;
+            }
+            k++;
+        }
+        return final;
+        wait(NULL);
+    }
 }
--- Projeto/hashing.c
+++ Projeto/hashing.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
@@ -9,7 +10,6 @@ int main(int argc, char* argv[])
     char* hash256final = "";
 
     snprintf(hash256final,62,"%d",execl("/usr/bin/sha256sum", "ls", argv[1], NULL));
-
 
     return 0;
 } 
