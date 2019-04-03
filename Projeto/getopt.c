3faa3a342600c89e525305915c8b708cda35c579
Fellipe <fellipe.ranheri@gmail.com>
2019-04-03 15:45:14 +0100

    getopt

--- Projeto/getopt.c
+++ Projeto/getopt.c
@@ -1,11 +1,54 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+
+char *filetype(char *opt)
+{
+  char *type;
+  char *erro="erro";
+  type=opt;
+  if( strcmp("md5", opt)== 0)
+  {
+    return type;
+  }
+  else if(strcmp("sha256 ", opt)== 0)
+  {
+  return type;
+  }
+  else if(strcmp("sha1 ", opt)== 0)
+  {
+  return type;
+  }
+  else if(strcmp("md5,sha1,sha256", opt)== 0)
+  {
+  return type;
+  }
+  else if(strcmp("sha1,sha256", opt)== 0)
+  {
+  return type;
+  }
+  else if(strcmp("md5,sha256", opt)== 0)
+  {
+  return type;
+  }
+  else if(strcmp("md5,sha1", opt)== 0)
+  {
+    return type;
+  }
+  else
+  {
+    return erro;
+  }
+
+}
 
 int main(int argc, char *argv[])
 {
 int c;
-
+char *ifile;
+extern char *optarg;
 while((c=getopt(argc,argv,"r:h:o:hov:")) != -1)
 {
 switch(c)
@@ -19,7 +62,12 @@ break;
 case 'h':// forensic -h sha1,sha256 hello.txt
         //  forensic -h md5 -o output.txt -v hello.txt
 // calcular uma ou mais “impressões digitais” dos ficheiros analisados. Pode pedir-se os algoritmos MD5,SHA1 ou SHA256; querendo-se mais do que um, separar os identificadores por vírgulas.
-printf("%s\n",optarg);
+if(filetype(optarg)== "erro")
+{
+  exit(0);
+}
+
+printf("%s\n",filetype(optarg));
 break;
 
 case 'v':
