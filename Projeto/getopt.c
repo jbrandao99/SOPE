b78f8b7f33b6941b48803cf1699747b8ee1fdcab
Jota <jbrandao@live.com.pt>
2019-04-04 22:59:03 +0100

    MAIN

--- Projeto/getopt.c
+++ Projeto/getopt.c
@@ -4,7 +4,7 @@
 #include <getopt.h>
 #include <string.h>
 
-char *filetype(char *opt)
+char *hashstring(char *opt)
 {
   char *type;
   char *erro = "erro";
--- Projeto/main.c
+++ Projeto/main.c
@@ -1,9 +1,287 @@
-#include "tratastring.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <getopt.h>
 
-int main()
+char *filetype(char *argv, char *funcao)
 {
-    char *teste;
-    teste = typestring("file.txt: ASCII text","file.txt");
-    printf("%s", teste);
-    return 0;
+    int link[2];
+    __pid_t pid;
+    char foo[4096];
+    char final[4096];
+    memset(foo, 0x00, 4096);
+    memset(final, 0x00, 4096);
+    pipe(link);
+    pid = fork();
+
+    if (pid == 0)
+    {
+        dup2(link[1], STDOUT_FILENO);
+        close(link[0]);
+        close(link[1]);
+        execlp(funcao, funcao, argv, NULL);
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        close(link[1]);
+        read(link[0], foo, sizeof(foo));
+
+        //////////////////////////////SETTING STRING///////////////////
+        if (strcmp("file", funcao) == 0)
+        {
+            for (unsigned int i = 0; i < (strlen(argv) + 2); i++)
+            {
+                foo[i] = '0';
+            }
+            unsigned int k = 0;
+            int t = 0;
+            while (k < strlen(foo))
+            {
+                if (foo[k] != '0' && foo[k] != '\n' && foo[k] != ',')
+                {
+                    final[t] = foo[k];
+                    t++;
+                    k++;
+                    continue;
+                }
+                k++;
+            }
+        }
+        else if (strcmp("sha1sum", funcao) == 0)
+        {
+            for (unsigned int i = 0; i < 40; i++)
+            {
+                final[i] = foo[i];
+            }
+        }
+        else if (strcmp("sha256sum", funcao) == 0)
+        {
+            for (unsigned int i = 0; i < 64; i++)
+            {
+                final[i] = foo[i];
+            }
+        }
+        else if (strcmp("md5sum", funcao) == 0)
+        {
+            for (unsigned int i = 0; i < 32; i++)
+            {
+                final[i] = foo[i];
+            }
+        }
+        //////////////////////////////SETTING STRING/////////////////////
+
+        wait(NULL);
+        return strdup(&final[0]);
+    }
+}
+
+void WriteOnFile(int argc, char *argv)
+{
+    int fd;
+    fd = open("file.txt", O_WRONLY | O_APPEND);
+    dup2(fd, STDOUT_FILENO);
+    WriteOnSTDOUT(argc, argv);
+    close(fd);
+}
+
+void WriteOnSTDOUT(int argc, char *argv)
+{
+    struct stat buf;
+    char tm[20]; //time modified
+    char tc[20]; //time created
+    char *type;
+    char *hash_sha1;
+    char *hash_sha256;
+    char *hash_md5;
+
+    if (argc == 2)
+    {
+        stat(argv, &buf);
+        strftime(tm, 20, "%Y-%m-%dT%H:%M:%S", localtime(&(buf.st_mtime)));
+        strftime(tc, 20, "%Y-%m-%dT%H:%M:%S", localtime(&(buf.st_ctime)));
+
+        printf("%s,", argv);
+        type = filetype(argv, "file");
+        printf("%s,", type);
+        printf("%ld,", buf.st_size);
+        printf((buf.st_mode & S_IRUSR) ? "r" : "");
+        printf((buf.st_mode & S_IWUSR) ? "w" : "");
+        printf((buf.st_mode & S_IXUSR) ? "x" : ",");
+        printf("%s,", tc);
+        printf("%s", tm);
+        printf(",");
+        hash_md5 = filetype(argv, "md5sum");
+        printf("%s,", hash_md5);
+        hash_sha1 = filetype(argv, "sha1sum");
+        printf("%s,", hash_sha1);
+        hash_sha256 = filetype(argv, "sha256sum");
+        printf("%s", hash_sha256);
+
+        printf("\n");
+    }
+    else
+    {
+        perror("No arguments!");
+    }
+}
+
+void isDirectory(char *argv)
+{
+    char *type = filetype(argv, "file");
+    if (strcmp("directory", type) == 0)
+    {
+        getDirectory(argv);
+    }
+    else
+        WriteOnSTDOUT(2, argv);
+}
+
+void getDirectory(char *argv)
+{
+    DIR *dir;
+    struct dirent *dent;
+    dir = opendir(argv);
+    pid_t pid;
+    char path[4096];
+    strcpy(path, argv);
+    char aux[4069];
+    if (dir != NULL)
+    {
+        while ((dent = readdir(dir)) != NULL)
+        {
+            if (strcmp(dent->d_name, ".") == 0 || strcmp(dent->d_name, "..") == 0)
+                continue;
+
+            strcpy(aux, path);
+            strcat(aux, "/");
+            strcat(aux, dent->d_name);
+            //printf("%s\n", aux);
+            //printf("%s\n", filetype(aux, "file"));
+            //printf("////////////////////////////////////////////////\n");
+            if (strcmp("directory", filetype(aux, "file")) != 0)
+            {
+                isDirectory(aux);
+                continue;
+            }
+
+            pid = fork();
+            if (pid == 0)
+            {
+                strcat(path, "/");
+                strcat(path, dent->d_name);
+                //printf("%s\n", path);
+                //printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+                getDirectory(path);
+                break;
+            }
+            else
+            {
+                wait(NULL);
+            }
+            //printf(dent->d_name);
+            //printf("\n");
+            //isDirectory(dent->d_name);
+        }
+    }
+    closedir(dir);
+}
+
+char *hashstring(char *opt)
+{
+  char *type;
+  char *erro = "erro";
+  type = opt;
+  if (strcmp("md5", opt) == 0)
+  {
+    //IMPRESSAO DE MD5
+    return type;
+  }
+  else if (strcmp("sha256 ", opt) == 0)
+  {
+    return type;
+  }
+  else if (strcmp("sha1 ", opt) == 0)
+  {
+    return type;
+  }
+  else if (strcmp("md5,sha1,sha256", opt) == 0)
+  {
+    return type;
+  }
+  else if (strcmp("sha1,sha256", opt) == 0)
+  {
+    return type;
+  }
+  else if (strcmp("md5,sha256", opt) == 0)
+  {
+    return type;
+  }
+  else if (strcmp("md5,sha1", opt) == 0)
+  {
+    return type;
+  }
+  else
+  {
+    return erro;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+    int c;
+    char *ifile;
+    extern char *optarg;
+    if (argc == 2)
+    {
+        //forensic hello.txt
+        printf("forensic hello.txt\n");
+    }
+    while ((c = getopt(argc, argv, "r:h:o:hov:")) != -1)
+    {
+        switch (c)
+        {
+        case 'r':
+            // forensic -r folder
+            // analisar todos os ficheiros do diretório indicado e de todos os seus subdiretórios.
+            printf("Analisando os ficheiros do diretorio: %s\n", optarg);
+            break;
+
+        case 'h': // forensic -h sha1,sha256 hello.txt
+                  //  forensic -h md5 -o output.txt -v hello.txt
+            // calcular uma ou mais “impressões digitais” dos ficheiros analisados. Pode pedir-se os algoritmos MD5,SHA1 ou SHA256; querendo-se mais do que um, separar os identificadores por vírgulas.
+            if (hashstring(optarg) == "erro")
+            {
+                exit(0);
+            }
+            printf("%s\n", hashstring(optarg));
+            break;
+
+        case 'v':
+            // gerar ficheiro com os registos de execução, conforme explicado mais à frente. O nome do ficheiro é obtido da variável de ambiente LOGFILENAME.
+            printf("Gerando ficheiros com os registros de execucao\n");
+            //printf("%s\n",getenv("LOGFILENAME"));
+            break;
+
+        case 'o':
+            //armazenar os dados da análise no ficheiro indicado (e não na saída padrão). O ficheiro terá, naturalmente,o formato CSV (comma-separated values), devido à forma como a informação é apresentada.
+            printf("Data saved on file %s,\n", optarg); //output.txt)
+            printf("Execution records saved on file ...\n");
+            break;
+
+        case '?':
+            break;
+
+        default: //forensic hello.txt
+            break;
+            return 0;
+        }
+    }
 }
