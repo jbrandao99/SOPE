c0a7b168480f78fa026770aa06bac249e4cad3ff
Jota <jbrandao@live.com.pt>
2019-05-14 15:00:45 +0100

    User started

---
+++ Projeto 2/user.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "constants.h"
+
+int user_id;
+
+int main(int argc, char* argv[])
+{
+    if (user_id == ADMIN_ACCOUNT_ID)
+    {
+        //admin
+    }
+    else
+    {
+        //client
+    }
+    
+    
+
+    return 0;
+}
