dd35b4bb2b4211f211fc21ba62d9f10f60e2884d
Jota <jbrandao@live.com.pt>
2019-05-14 22:27:26 +0100

    Server FIFO created

--- Projeto 2/server.c
+++ Projeto 2/server.c
@@ -1,4 +1,10 @@
 #include "sope.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 bank_account_t bank_accounts[MAX_BANK_ACCOUNTS];
 int num_accounts;
@@ -9,8 +15,35 @@ bool login(uint32_t id, char* pass)
     {
         if(bank_accounts[i].account_id == id)
         {
+            //falta password (hash)
             return true;
         }
     }
     return false;
 }
+
+void createServerFIFO()
+{
+    if(mkfifo(SERVER_FIFO_PATH, 0660) < 0)
+    {
+        if(errno == EEXIST)
+        {
+        printf("FIFO %s alreay exists\n", SERVER_FIFO_PATH);
+        }
+        else
+        {
+            printf("Can't create FIFO\n");
+        }
+        
+        if(unlink(SERVER_FIFO_PATH) < 0)
+        {
+            printf("Error when destroying FIFO %s\n",SERVER_FIFO_PATH);
+        }
+        else
+        {
+            printf("FIFO %s has been destroyed\n", SERVER_FIFO_PATH);
+        }
+        exit(0);
+    }
+}
+
