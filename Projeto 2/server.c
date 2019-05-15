8a8f89fcb9425a76624e8bc26cec2482d3609556
Jota <jbrandao@live.com.pt>
2019-05-15 23:54:35 +0100

    Finished opt part

--- Projeto 2/server.c
+++ Projeto 2/server.c
@@ -6,9 +6,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 bank_account_t bank_accounts[MAX_BANK_ACCOUNTS];
-int num_accounts;
+int num_accounts = 0;
 int fd, fd_dummy;
 int num_bancos;
 int num_password;
@@ -115,9 +116,31 @@ void verifyArgs(char *argv[])
     }
 }
 
-//create salt char array in main to put as argument
-char *getSalt(char *salt)
+char *getHash(char *password, char *salt)
 {
+
+    char cmd[1024] = "echo -n ";
+    strcat(cmd, password);
+    strcat(cmd, salt);
+    strcat(cmd, " | sha256sum");
+
+    FILE *fp;
+    fp = popen(cmd, "r");
+    if (fp == NULL)
+    {
+        printf("Failed to run command\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char output[1024];
+    fscanf(fp, "%[^\n]", output);
+    pclose(fp);
+    return strdup(&output[0]);
+}
+
+char *getSalt()
+{
+    char *salt = (char *)malloc((SALT_LEN + 1) * sizeof(char));
     unsigned int i;
     for (i = 0; i < SALT_LEN; i++)
     {
@@ -127,4 +150,94 @@ char *getSalt(char *salt)
     salt[i] = '\0';
 
     return salt;
+}
+
+int createAccount(uint32_t id, uint32_t balance, char *password)
+{
+    bank_account_t bank_account;
+    char *salt;
+
+    if (isAccount(id))
+    {
+        return RC_ID_IN_USE;
+    }
+
+    bank_account.account_id = id;
+    bank_account.balance = balance;
+    salt = getSalt();
+    strcpy(bank_account.salt, salt);
+    strcpy(bank_account.hash, getHash(password, salt));
+    addAccount(bank_account);
+    return RC_OK;
+}
+
+void addAccount(bank_account_t bank_account)
+{
+    bank_accounts[num_accounts] = bank_account;
+    num_accounts++;
+}
+
+bool isAccount(uint32_t id)
+{
+    for (unsigned int i = 0; i < num_accounts; i++)
+    {
+        if (bank_accounts[i].account_id == id)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int transfer(uint32_t src_id, uint32_t dest_id, uint32_t amount)
+{
+    if (!isAccount(src_id))
+    {
+        return RC_OTHER;
+    }
+
+    if (!isAccount(dest_id))
+    {
+        return RC_ID_NOT_FOUND;
+    }
+
+    if (src_id == dest_id)
+    {
+        return RC_SAME_ID;
+    }
+
+    bank_account_t *src_account, *dest_account;
+    src_account = getAccount(src_id);
+    dest_account = getAccount(dest_id);
+
+    if (src_account->balance < amount)
+    {
+        return RC_NO_FUNDS;
+    }
+
+    if (dest_account->balance + amount > MAX_BALANCE)
+    {
+        return RC_TOO_HIGH;
+    }
+
+    src_account->balance -= amount;
+    dest_account->balance += amount;
+
+    return RC_OK;
+}
+
+bank_account_t *getAccount(uint32_t id)
+{
+    bank_account_t *bank_account = (bank_account_t *)malloc(sizeof(bank_account_t));
+    bank_account = NULL;
+
+    for (unsigned int i = 0; i < num_accounts; i++)
+    {
+        if (bank_accounts[i].account_id == id)
+        {
+            bank_account = &bank_accounts[i];
+            break;
+        }
+    }
+    return bank_account;
 }
