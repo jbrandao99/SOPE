007436dd0d6d4fc58b1406df97402d8379b345a8
Jota <jbrandao@live.com.pt>
2019-05-15 11:00:14 +0100

    Server cases

--- Projeto 2/server.c
+++ Projeto 2/server.c
@@ -1,6 +1,7 @@
 #include "sope.h"
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +9,7 @@
 
 bank_account_t bank_accounts[MAX_BANK_ACCOUNTS];
 int num_accounts;
+int fd, fd_dummy;
 
 bool login(uint32_t id, char* pass)
 {
@@ -54,4 +56,45 @@ void destroyServerFIFO()
             exit(EXIT_SUCCESS);
         }
 }
+
+void openServerFIFO()
+{
+    if((fd = open(SERVER_FIFO_PATH, O_RDONLY)) != -1)
+    {
+        printf("FIFO %s openned in READONLY mode\n", SERVER_FIFO_PATH);
+    }
+    
+    if((fd_dummy = open(SERVER_FIFO_PATH, O_WRONLY)) != -1)
+    {
+        printf("FIFO %s openned in WRITEONLY mode\n", SERVER_FIFO_PATH);
+    }
+}
+
+void closeServerFIFO()
+{
+    close(fd);
+    close(fd_dummy);
+}
+
+void shutdown()
+{
+    chmod(SERVER_FIFO_PATH, S_IRUSR|S_IRGRP|S_IROTH);
+}
+
+int balance(uint32_t id)
+{
+    bank_account_t bank_account;
+    for (unsigned int i = 0; i < num_accounts; i++)
+    {
+        if(bank_accounts[i].account_id == id)
+        {
+            bank_account = bank_accounts[i];
+            break;
+        }
+    }
+    return bank_account.balance;
+}
+
+
+
 
