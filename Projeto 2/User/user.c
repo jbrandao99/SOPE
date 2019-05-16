cb7df583ca0da444a0046d45f7dc977c0b1dae90
Jota <jbrandao@live.com.pt>
2019-05-16 18:45:41 +0100

    User started

--- Projeto 2/User/user.c
+++ Projeto 2/User/user.c
@@ -7,11 +7,11 @@
 #include <unistd.h>
 #include <string.h>
 
-int num_accounts;
+int num_account;
 int num_delay;
 int op_number;
-req_header_t user;
-req_value_t value;
+char size_pass[MAX_PASSWORD_LEN + 1];
+req_header_t req_header;
 
 void verifyArgs(int argc, char *argv[])
 {
@@ -22,14 +22,13 @@ void verifyArgs(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
 
-  num_accounts = atoi(argv[1]);
-  if (num_accounts > MAX_BANK_ACCOUNTS || num_accounts < 0)
+  num_account = atoi(argv[1]);
+  if (num_account > MAX_BANK_ACCOUNTS || num_account < 0)
   {
     printf("Invalid Number of Accounts: %d\n", MAX_BANK_ACCOUNTS);
     exit(EXIT_FAILURE);
   }
 
-  char size_pass[MAX_PASSWORD_LEN + 1];
   strcpy(size_pass, argv[2]);
   if (strlen(size_pass) < MIN_PASSWORD_LEN || strlen(size_pass) > MAX_PASSWORD_LEN)
   {
@@ -53,58 +52,18 @@ void verifyArgs(int argc, char *argv[])
   }
 }
 
-void sincronizeUser(char *args[]) //FALTA PID
+void sincronizeHeader()
 {
-  user.account_id = atoi(args[1]);
-  strcpy(user.password, args[2]);
-  user.op_delay_ms = atoi(args[3]);
-}
-
-void adminAcess(int code)
-{
-  if (code == OP_CREATE_ACCOUNT)
-  {
-    //
-  }
-  if (code == OP_SHUTDOWN)
-  {
-    //
-  }
-  else
-  {
-    printf("Code only available to user acess %d\n", code);
-    exit(1);
-  }
-}
-
-void userAcess(int code)
-{
-  if (code == OP_BALANCE)
-  {
-    //
-  }
-  if (code == OP_TRANSFER)
-  {
-    //
-  }
-  else
-  {
-    printf("Code only available to client acess %d\n", code);
-    exit(1);
-  }
+    req_header.pid = getpid();
+    req_header.account_id = num_account;
+    strcpy(req_header.password, size_pass);
+    req_header.op_delay_ms = num_delay;
 }
 
 int main(int argc, char *argv[])
 {
 
   verifyArgs(argc, argv);
-  sincronizeUser(argv);
-  if (num_accounts == ADMIN_ACCOUNT_ID)
-  {
-    adminAcess(num_operacao);
-  }
-  else
-  {
-    userAcess(num_operacao);
-  }
+
+  sincronizeHeader();
 }
