2886462826fe491bed2aa6b9bab9bf3b20295ae7
Fellipe <fellipe.ranheri@gmail.com>
2019-05-16 13:15:32 +0100

    user.c atualizada

--- Projeto 2/user.c
+++ Projeto 2/user.c
@@ -8,23 +8,41 @@
 
 int num_password;
 int num_accounts;
+int num_delay;
+int num_operacao;
 
 
 //FAZ NA MAIN POR ISSO E QUE TEM ARGV
 void verifyArgs(char *args[]){
 
-    num_password= strlen(argv[2]);    //Tamanho da pass
+   num_accounts=atoi(args[1]);
+   if(num_accounts>MAX_BANK_ACCOUNTS || num_accounts<1)
+   {
+     printf("Numero invalido de conta %d\n",MAX_BANK_ACCOUNTS);
+     exit(1);
+   }
+
+    num_password= strlen(args[2]);    //Tamanho da pass
     if(num_password < MIN_PASSWORD_LEN || num_password > MAX_PASSWORD_LEN){
         printf("Password invalida, tente com %d a %d caracteres\n", MIN_PASSWORD_LEN, MAX_PASSWORD_LEN);
         exit(1);
     }
 
-    num_accounts=atoi(args[1]);
-    if(num_accounts>MAX_BANK_ACCOUNTS || num_accounts<=0)
+    num_delay = atoi(args[3]);
+    if(num_delay>MAX_OP_DELAY_MS || num_delay<1)
     {
-      printf("Numero invalido de conta %d\n",MAX_BANK_ACCOUNTS);
+      printf("Delay invalido %d\n", MAX_OP_DELAY_MS);
       exit(1);
     }
+
+    num_operacao = atoi(args[4]);
+    if(num_operacao<0 || num_operacao>3)
+    {
+    printf("Codigo de operacao invalido %d\n",num_operacao);
+    exit(1);
+    }
+
+
 
 
 }
