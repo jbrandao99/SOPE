ddf746fe172a5d29f1e93a8bbbebce5e43f69857
Fellipe <fellipe.ranheri@gmail.com>
2019-05-16 16:07:59 +0100

    user.c

--- Projeto 2/user.c
+++ Projeto 2/user.c
@@ -10,6 +10,8 @@ int num_password;
 int num_accounts;
 int num_delay;
 int num_operacao;
+req_header_t user;
+req_value_t value;
 
 
 //FAZ NA MAIN POR ISSO E QUE TEM ARGV
@@ -41,6 +43,78 @@ void verifyArgs(char *args[]){
     printf("Codigo de operacao invalido %d\n",num_operacao);
     exit(1);
     }
+
+}
+
+void sincronizeUser(char *args[])  //FALTA PID
+{
+  user.account_id= atoi(args[1]);
+  strcpy(user.password,args[2]);
+  user.op_delay_ms=atoi(args[3]);
+
+}
+
+void adminAcess(int code)
+{
+  if(code==OP_CREATE_ACCOUNT)
+  {
+    //
+  }
+  if(code==OP_SHUTDOWN)
+  {
+    //
+  }
+  else
+  {
+    printf("Code only available to user acess %d\n",code);
+    exit(1);
+  }
+
+
+}
+
+void userAcess(int code)
+{
+  if(code==OP_BALANCE)
+  {
+    //
+  }
+  if(code==OP_TRANSFER)
+  {
+    //
+  }
+  else
+  {
+    printf("Code only available to client acess %d\n",code);
+    exit(1);
+  }
+
+
+}
+
+
+
+
+int main(int argc, char *argv[]) {
+
+if(argc!=6)
+{
+  exit(1);
+}
+verifyArgs(argv);
+sincronizeUser(argv);
+if(num_accounts==ADMIN_ACCOUNT_ID)
+{
+  adminAcess(num_operacao);
+}
+else
+{
+  userAcess(num_operacao);
+}
+
+
+
+
 
 
 
